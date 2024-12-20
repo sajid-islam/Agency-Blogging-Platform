2520dcc9be335cb027da8375dd4029f29a7ccbea
sajid941 <sajidislam941@gmail.com>
2024-12-20 08:08:13 +0600

    Add: Design about page cover section

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const AboutPage = () => {
-  return (
-    <div>AboutPage</div>
-  )
-}
+    return (
+        <main>
+            <section className="mt-10">
+                <div className="relative w-full h-[300px]">
+                    <Image
+                        className="object-cover grayscale rounded-md"
+                        src="/images/about.jpg"
+                        fill={true}
+                        alt="about page cover image"
+                    />
+                    <div className="p-3 bg-primary-500 rounded-md absolute text-white bottom-5 left-5">
+                        <h3 className="text-2xl font-bold">
+                            Digital storytellers
+                        </h3>
+                        <p className="text-xl font-semibold">
+                            Handcrafting award winning digital experiences
+                        </p>
+                    </div>
+                </div>
+            </section>
+        </main>
+    );
+};
 
-export default AboutPage
+export default AboutPage;
