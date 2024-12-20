e0d0c1022a9c621836cf9e10b4e436fc54591755
sajid941 <sajidislam941@gmail.com>
2024-12-20 07:33:14 +0600

    Add: Design home page hero section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,32 @@
 import Image from "next/image";
+// import HeroImg from "/images/hero.svg"
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return (
-    <main>
-      Home
-    </main>
-  );
+    return (
+        <main>
+            <section className="flex gap-10 items-center my-10">
+                <div className="flex-1 space-y-7">
+                    <h1 className="text-6xl font-bold leading-[1.25] bg-gradient-to-t to-primary-600 from-gray-200 bg-clip-text text-transparent">
+                        Creating Better Designs for Your Digital Ideas
+                    </h1>
+                    <p className="text-gray-text">
+                        Collaborating with the best minds in tech to turn your
+                        ideas into impactful solutions.
+                    </p>
+                    <Button className="bg-primary-500 hover:bg-primary-400 font-semibold text-white">
+                        See Our Website
+                    </Button>
+                </div>
+                <div className="flex-1 flex justify-center ">
+                    <Image
+                        src={"/images/hero.svg"}
+                        width={400}
+                        height={400}
+                        alt="Home page hero image"
+                    />
+                </div>
+            </section>
+        </main>
+    );
 }
