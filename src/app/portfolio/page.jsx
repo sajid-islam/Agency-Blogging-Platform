ce5aeeff8a23834ec9960e018548975ffc570764
sajid941 <sajidislam941@gmail.com>
2024-12-21 18:13:47 +0600

    update: instant of using image path use import

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,5 +1,9 @@
 import Link from "next/link";
 import React from "react";
+import applicationImg from "@/images/application.jpg";
+import illustrationImg from "@/images/illustration.jpg";
+import websiteImg from "@/images/website.jpg";
+import Image from "next/image";
 
 const PortfolioPage = () => {
     return (
@@ -12,6 +16,13 @@ const PortfolioPage = () => {
                     href="/portfolio/application"
                     className="relative w-full md:w-[300px] h-[400px] text-white hover:text-primary-500 p-10 border-4 rounded-sm border-black bg-[url('/images/application.jpg')] bg-cover bg-no-repeat"
                 >
+                    <Image
+                        className="object-cover"
+                        src={applicationImg}
+                        placeholder="blur"
+                        alt="category application image"
+                        fill={true}
+                    />
                     <span className="text-2xl font-bold absolute right-3 bottom-3">
                         Application
                     </span>
@@ -20,6 +31,14 @@ const PortfolioPage = () => {
                     className="relative w-full md:w-[300px] h-[400px] text-white hover:text-primary-500 p-10 border-4 rounded-sm border-black bg-[url('/images/illustration.jpg')] bg-cover bg-no-repeat"
                     href="/portfolio/illustrations"
                 >
+                    <Image
+                        className="object-cover"
+                        src={illustrationImg}
+                        placeholder="blur"
+                        alt="category application image"
+                        fill={true}
+                    />
+
                     <span className="text-2xl font-bold absolute right-3 bottom-3">
                         Illustrations
                     </span>
@@ -28,6 +47,14 @@ const PortfolioPage = () => {
                     className="relative w-full md:w-[300px] h-[400px] text-white hover:text-primary-500 p-10 border-4 rounded-sm border-black bg-[url('/images/website.jpg')] bg-cover bg-no-repeat"
                     href="/portfolio/website"
                 >
+                    <Image
+                        className="object-cover"
+                        src={websiteImg}
+                        placeholder="blur"
+                        alt="category application image"
+                        fill={true}
+                    />
+
                     <span className="text-2xl font-bold absolute right-3 bottom-3">
                         Websites
                     </span>
