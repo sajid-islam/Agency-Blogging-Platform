393b2c040b69aca07a900dde85cd710d3d5d8b34
sajid islam <sajidisalm941@gmail.com>
2024-12-23 15:43:21 +0600

    featch dummy json from jsonplaceholder.typicode.com and show on blog page

--- src/app/blogs/[id]/page.jsx
+++ src/app/blogs/[id]/page.jsx
@@ -3,18 +3,17 @@ import userLight from "@/images/userLight.png";
 import userDark from "@/images/userDark.png";
 import Image from "next/image";
 
-const BlogPost = () => {
+const BlogPost =async ({params}) => {
+    const id = await params.id;
+    const data = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+    const blog = await data.json();
     return (
         <main>
             <section className="flex flex-col md:flex-row items-center gap-10 mt-10">
                 <div className="space-y-5 md:flex-1">
-                    <h1 className="text-3xl font-bold">Blog 1</h1>
+                    <h1 className="text-3xl font-bold">{blog.title}</h1>
                     <p className="text-gray-text">
-                        Lorem ipsum dolor sit, amet consectetur adipisicing
-                        elit. Libero, vel, ad ut dolorem perferendis asperiores
-                        itaque, labore quaerat voluptate sapiente dolor cum
-                        incidunt impedit quisquam quos optio! Quidem, odit
-                        voluptates.
+                        {blog.body.slice(0, 50)}...
                     </p>
                     <div className="flex items-center gap-2">
                         <Image
@@ -39,61 +38,7 @@ const BlogPost = () => {
             </section>
             <section className="mt-8">
                 <p className="text-[18px] text-gray-text text-justify">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Architecto voluptatibus quisquam eligendi dolorem error, sed
-                    laudantium dignissimos esse maiores adipisci impedit
-                    accusantium. Officiis magni tempora provident sint molestiae
-                    placeat aspernatur impedit autem alias dolor ducimus vel,
-                    cumque dolorem quod, deleniti ratione quasi explicabo quas
-                    ipsa minus fugit maiores, inventore ut ad? Quaerat ipsum in
-                    deserunt minima blanditiis ut necessitatibus, repellat
-                    aspernatur animi neque libero totam praesentium et,
-                    asperiores distinctio fugit quas consequatur. Maxime
-                    voluptate sapiente suscipit non dolore optio laborum
-                    nesciunt cupiditate quia modi officiis aspernatur, velit,
-                    accusamus tempore eveniet vero! Vel quo impedit eaque libero
-                    natus aperiam numquam rem, quod explicabo animi eius rerum
-                    ad et facere. Error possimus esse quisquam similique.
-                    Voluptatum minus sequi saepe dolores at delectus, et magni
-                    eum accusamus suscipit velit officia vel sit ipsum. Ex
-                    quibusdam nam in quia maxime, quod optio illo consectetur
-                    quisquam et perspiciatis saepe quis ut accusamus. Aut
-                    necessitatibus provident, doloremque aperiam impedit illo ab
-                    ratione obcaecati assumenda officia ullam quibusdam iusto,
-                    aliquid, debitis magnam voluptates voluptate? Minima iure
-                    natus facere iusto similique in accusantium at aut commodi
-                    id, et alias error necessitatibus, minus harum pariatur
-                    cumque neque. Perspiciatis odio quod similique eaque ad,
-                    repudiandae, reiciendis sapiente eveniet tenetur deleniti
-                    quia ullam beatae earum distinctio vero facilis, corporis
-                    minus asperiores accusantium autem illum hic voluptatem?
-                    Quidem ipsa sint commodi suscipit alias tempora ratione
-                    ipsum. Maxime perspiciatis magni reprehenderit animi,
-                    similique doloribus exercitationem aliquam dolorum sequi
-                    amet natus nobis, neque asperiores aperiam deserunt iusto
-                    necessitatibus vitae! Numquam, labore tempora. Quidem aut
-                    ullam accusantium mollitia modi ducimus nesciunt obcaecati
-                    tempora accusamus perferendis magnam ut at culpa, molestias
-                    quia cupiditate maxime incidunt eveniet provident inventore
-                    sequi quisquam rerum! Ipsa velit rerum, voluptatem
-                    distinctio nihil architecto dolores asperiores eum ex,
-                    possimus incidunt perferendis adipisci quasi reprehenderit
-                    reiciendis error corporis, quas in nemo nesciunt est
-                    doloribus! Corrupti veritatis eligendi, reprehenderit, fugit
-                    quisquam qui quis quod eaque omnis, consequatur molestias.
-                    Eos natus facere rerum sed, minus autem, harum sequi
-                    pariatur, perferendis distinctio optio! Nesciunt
-                    exercitationem commodi soluta magnam praesentium. Pariatur,
-                    repellendus, atque dignissimos distinctio delectus natus
-                    ipsam sint adipisci similique possimus velit numquam
-                    necessitatibus porro? Quam tempore error ipsa quidem
-                    excepturi reprehenderit odit, inventore voluptatum nobis
-                    dicta molestias deleniti similique et minus repellat,
-                    quibusdam assumenda! Laudantium, aliquam? Dolorem quisquam
-                    aliquid, recusandae accusamus praesentium beatae odit earum.
-                    Blanditiis sit placeat quam rem, nulla aspernatur corrupti,
-                    dignissimos nemo nostrum nobis dolore error. Voluptatum
-                    totam facilis nemo recusandae nam?
+                    {blog.body}
                 </p>
             </section>
         </main>
--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -2,110 +2,34 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const BlogPage = () => {
+const BlogPage = async () => {
+    const data = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const blogs = await data.json();
     return (
         <main>
             <section className="mt-10">
-                <Link href="/blogs/5" className="flex flex-col md:flex-row items-center gap-10 mb-10">
-                    <div className="w-full md:w-[500px] h-[200px] relative">
-                        <Image
-                            className="object-cover"
-                            src="/images/website.jpg"
-                            alt="blog image"
-                            fill={true}
-                        />
-                    </div>
-                    <div className="space-y-3">
-                        <h1 className="text-3xl font-bold">Blog 1</h1>
-                        <p className="text-gray-text">
-                            Lorem ipsum dolor sit, amet consectetur adipisicing
-                            elit. Libero, vel, ad ut dolorem perferendis
-                            asperiores itaque, labore quaerat voluptate sapiente
-                            dolor cum incidunt impedit quisquam quos optio!
-                            Quidem, odit voluptates.
-                        </p>
-                    </div>
-                </Link>
-                <Link href="/blogs/1" className="flex flex-col md:flex-row items-center gap-10 mb-10">
-                    <div className="w-full md:w-[500px] h-[200px] relative">
-                        <Image
-                            className="object-cover"
-                            src="/images/website.jpg"
-                            alt="blog image"
-                            fill={true}
-                        />
-                    </div>
-                    <div className="space-y-3">
-                        <h1 className="text-3xl font-bold">Blog 1</h1>
-                        <p className="text-gray-text">
-                            Lorem ipsum dolor sit, amet consectetur adipisicing
-                            elit. Libero, vel, ad ut dolorem perferendis
-                            asperiores itaque, labore quaerat voluptate sapiente
-                            dolor cum incidunt impedit quisquam quos optio!
-                            Quidem, odit voluptates.
-                        </p>
-                    </div>
-                </Link>
-                <Link href="/blogs/2" className="flex flex-col md:flex-row items-center gap-10 mb-10">
-                    <div className="w-full md:w-[500px] h-[200px] relative">
-                        <Image
-                            className="object-cover"
-                            src="/images/website.jpg"
-                            alt="blog image"
-                            fill={true}
-                        />
-                    </div>
-                    <div className="space-y-3">
-                        <h1 className="text-3xl font-bold">Blog 1</h1>
-                        <p className="text-gray-text">
-                            Lorem ipsum dolor sit, amet consectetur adipisicing
-                            elit. Libero, vel, ad ut dolorem perferendis
-                            asperiores itaque, labore quaerat voluptate sapiente
-                            dolor cum incidunt impedit quisquam quos optio!
-                            Quidem, odit voluptates.
-                        </p>
-                    </div>
-                </Link>
-                <Link href="/blogs/3" className="flex flex-col md:flex-row items-center gap-10 mb-10">
-                    <div className="w-full md:w-[500px] h-[200px] relative">
-                        <Image
-                            className="object-cover"
-                            src="/images/website.jpg"
-                            alt="blog image"
-                            fill={true}
-                        />
-                    </div>
-                    <div className="space-y-3">
-                        <h1 className="text-3xl font-bold">Blog 1</h1>
-                        <p className="text-gray-text">
-                            Lorem ipsum dolor sit, amet consectetur adipisicing
-                            elit. Libero, vel, ad ut dolorem perferendis
-                            asperiores itaque, labore quaerat voluptate sapiente
-                            dolor cum incidunt impedit quisquam quos optio!
-                            Quidem, odit voluptates.
-                        </p>
-                    </div>
-                </Link>
-                <Link href="/blogs/4" className="flex flex-col md:flex-row items-center gap-10 mb-10">
-                    <div className="w-full md:w-[500px] h-[200px] relative">
-                        <Image
-                            className="object-cover"
-                            src="/images/website.jpg"
-                            alt="blog image"
-                            fill={true}
-                        />
-                    </div>
-                    <div className="space-y-3">
-                        <h1 className="text-3xl font-bold">Blog 1</h1>
-                        <p className="text-gray-text">
-                            Lorem ipsum dolor sit, amet consectetur adipisicing
-                            elit. Libero, vel, ad ut dolorem perferendis
-                            asperiores itaque, labore quaerat voluptate sapiente
-                            dolor cum incidunt impedit quisquam quos optio!
-                            Quidem, odit voluptates.
-                        </p>
-                    </div>
-                </Link>
+                {blogs.map((blog) => (
+                    <Link
+                        key={blog.id}
+                        href={`/blogs/${blog.id}`}
+                        className="flex flex-col md:flex-row items-center gap-10 mb-10"
+                    >
+                        <div className="w-full md:w-[300px] h-[200px] relative">
+                            <Image
+                                className="object-cover"
+                                src="/images/website.jpg"
+                                alt="blog image"
+                                fill={true}
+                            />
+                        </div>
+                        <div className="space-y-3 flex-1">
+                            <h1 className="text-3xl font-bold">{blog.title}</h1>
+                            <p className="text-gray-text">
+                                {blog.body.slice(0, 100)}...
+                            </p>
+                        </div>
+                    </Link>
+                ))}
             </section>
         </main>
     );
