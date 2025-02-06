7e1ae00da62904fe4f608103ebb8ffff296c03ab
sajid islam <sajidisalm941@gmail.com>
2025-02-06 19:25:01 +0600

    fix naming problem of main layout

--- src/app/(main)/layout.js
+++ src/app/(main)/layout.js
@@ -1,7 +1,7 @@
 import Footer from "@/components/footer/Footer";
 import Navbar from "@/components/navbar/Navbar";
 
-export default function RootLayout({ children }) {
+export default function MainLayout({ children }) {
     return (
         <div className="max-w-6xl mx-auto px-5">
             <nav>
